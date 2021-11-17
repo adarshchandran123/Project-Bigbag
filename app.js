277e64a5451ac15dd890babb179e9be310c592c1
Adarsh Chandran <adarshchandran555@gmail.com>
2021-11-17 13:01:58 +0530

    Cart completed

--- app.js
+++ app.js
@@ -87,9 +87,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  console.log('1x',err.message);
-  console.log('1xggggggggggggg',res.locals.message);
-  console.log('455555555555555555555',err.status);
+
 
   // render the error page
   res.status(err.status || 500);
